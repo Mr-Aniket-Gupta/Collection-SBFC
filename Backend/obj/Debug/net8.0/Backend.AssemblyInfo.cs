d40983c109725d0f102c53d285c0e546ced3c546
Mr-Aniket-Gupta <aniketgupta2226@gmail.com>
2026-07-02 12:41:36 +0530

    all set

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02633fbf6633ac81ffbfbb82f09689362696a234")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0594f00eedc47a981e54f52ddff375843805c7a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
